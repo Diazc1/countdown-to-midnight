a441a5995f13df689eff934a629f052b11f065fe
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-08 09:24:23 -0800

    commit tree bf26362e4854f167372aec86c3bf3956f30aa7c2

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)      #takes in an integer arguement and uses a while loop to countdown from that integer to 0
+    while number > 0 do
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end 
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+    while number > 0 do
+        puts "#{number} SECOND(S)!"
+        sleep(5)     #the number 5 is the number of seconds it takes to execute the puts "sleep(num_secs)"
+        number -= 1
+    end
+    "HAPPY NEW YEAR"
+end
+
+    
